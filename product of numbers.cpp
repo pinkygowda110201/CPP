e121db78f95d335b884914cadd7a3ae3033cb892
pinkygowda110201 <71083770+pinkygowda110201@users.noreply.github.com>
2020-10-18 21:36:28 +0530

    Add files via upload

---
+++ product of numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float a,b;
+	cout<<"Enter two numbers:"<<endl;
+	cin>>a;
+	cin>>b;
+	cout<<"Product = "<<(a*b)<<endl;
+	return 0;
+}
---
+++ size of data types.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+main()
+{
+	cout<<"Size of char: "<<sizeof(char)<<" byte"<<endl;
+	cout<<"Size of int: "<<sizeof(int)<<" bytes"<<endl;
+	cout<<"Size of float: "<<sizeof(float)<<" bytes\n";	
+	cout<<"Size of double: "<<sizeof(double)<<" bytes\n";
+}
