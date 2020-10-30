e3f678a8815db51257ffb0c37336ad573f05c7f4
pinkygowda110201 <71083770+pinkygowda110201@users.noreply.github.com>
2020-10-30 21:48:28 +0530

    Add files via upload

---
+++ Time.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+ using namespace std;
+ class Time
+{
+    private:
+        int seconds;
+        int hh,mm,ss;
+    public:
+        void getTime(void);
+		void display(void);
+}; 
+void Time::getTime(void)
+{
+    cout << "Enter time:" << endl;
+    cout << "Hours?   ";          
+	cin >> hh;
+    cout << "Minutes? ";          
+	cin >> mm;
+    cout << "Seconds? ";          
+	cin >> ss;
+} 
+void Time::display(void)
+{
+    cout << "The time is = "<<hh<<":"<<mm<<":"<<ss<<endl; 
+    cout << "Time in total seconds: " <<hh*3600+mm*60+ss;
+} 
+int main()
+{
+    Time T;
+    T.getTime();
+    T.display();
+    return 0;
+}
